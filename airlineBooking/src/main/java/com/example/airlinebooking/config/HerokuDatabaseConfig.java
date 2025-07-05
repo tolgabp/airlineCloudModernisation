820fc99f18efcb28f9d8bdca55fea5c94633a94f
tolgabp <tolgabarisp@gmail.com>
2025-07-05 22:35:02 +0100

    Deploy airline booking to Heroku

---
+++ airlineBooking/src/main/java/com/example/airlinebooking/config/HerokuDatabaseConfig.java
@@ -0,0 +1,44 @@
+package com.example.airlinebooking.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+@Profile("heroku")
+public class HerokuDatabaseConfig {
+
+    @Value("${DATABASE_URL:}")
+    private String databaseUrl;
+
+    @Bean
+    @ConditionalOnProperty(name = "DATABASE_URL")
+    public DataSource dataSource() throws URISyntaxException {
+        if (databaseUrl == null || databaseUrl.isEmpty()) {
+            throw new IllegalStateException("DATABASE_URL environment variable is not set");
+        }
+
+        URI dbUri = new URI(databaseUrl);
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setValidationQuery("SELECT 1");
+
+        return dataSource;
+    }
+} 
